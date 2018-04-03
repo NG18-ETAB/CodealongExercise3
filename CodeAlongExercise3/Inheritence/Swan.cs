0d5f1fc7883491eae2babfee30f1d7621bc7f149
Linda Pious Raj <lintroji@hotmail.com>
2018-04-03 11:03:59 +0200

    Finish Codealong Exercise3.2-Inheritence

---
+++ CodeAlongExercise3/Inheritence/Swan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3
+{
+    public class Swan:Bird
+    {
+        public Swan(int age, string name, double weight,bool hasMate) :base(age, name, weight)
+        {
+            HasMate = hasMate;
+        }
+
+        public bool HasMate { get; set; }
+
+    }
+
+
+}
