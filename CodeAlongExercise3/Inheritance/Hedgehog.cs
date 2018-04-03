94c8023ef3e2ada02372fbec72f4363f3c9de8b1
Yvette85 <y.kamariza@yahoo.com>
2018-04-03 11:07:03 +0200

    Finished codealong on 3.2 -Inheritance

---
+++ CodeAlongExercise3/Inheritance/Hedgehog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3.Inheritance
+{
+    class Hedgehog : Animal
+    {
+
+        public Hedgehog(int age, string name, double weight, int numberOfRings) :
+            base(age, name, weight)
+        {
+            NumberOfRings = numberOfRings;
+        }
+        public int NumberOfRings { get; set; }
+    }
+}
---
+++ CodeAlongExercise3/Inheritance/Horse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3
+{
+    public class Horse : Animal
+    {
+        public Horse(int age, string name, double weight, double speed) :
+            base(age, name, weight) // constructor//
+        {
+            Speed = speed;
+        }
+        public double Speed { get; set; }
+
+    }
+}
+
