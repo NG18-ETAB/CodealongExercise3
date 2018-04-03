41ce8927f7babf4fa3b886eb91bc59710ffe30df
Xuan Ke <cathy085@gmail.com>
2018-04-03 10:54:47 +0200

    Finished codealong on 3.2 - Inheritance

---
+++ CodeAlongExercise3/Inheritance/Horse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3
+{
+    public class Horse : Animal
+    {
+        public Horse(int age, string name, double weight, double speed) : base(age, name, weight)
+        {
+            Speed = speed;
+        }
+
+        public double Speed { get; set; }
+    }
+}
