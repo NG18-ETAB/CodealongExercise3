ffb185ef5216803a8a46ed77cf8edf9493e1b422
John Hellman <acoppero@gmail.com>
2018-04-03 10:54:47 +0200

    Finished codealong on 3.2 - Inheritance

---
+++ CodeAlongExercise3/Inheritance/Pelican.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3
+{
+    public class Pelican : Bird
+    {
+        public Pelican(int age, string name,
+            double weight, string plummage,
+            double beakVolume) :
+            base(age, name, weight, plummage)
+        {
+            BeakVolume = beakVolume;
+        }
+
+        public double BeakVolume { get; set; }
+    }
+}
---
+++ CodeAlongExercise3/Inheritance/Swan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3
+{
+    public class Swan : Bird
+    {
+        public Swan(int age, string name,
+            double weight, string plummage, bool hasMate) :
+            base(age, name, weight, plummage)
+        {
+            HasMate = hasMate;
+        }
+
+        public bool HasMate { get; set; }
+    }
+}
