efe8bc798ca7a79600a36fd49bdb83a0237439b8
zafar iqbal <rasalunic@gmail.com>
2018-04-03 10:54:37 +0200

    Finished codealong on 3.2 - Inheritance

---
+++ CodeAlongExercise3/Inheritance/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3
+{
+    public class Animal
+    {
+        public Animal(int age, string name, double weight)
+        {
+            Age = age;
+            Name = name;
+            Weight = weight;
+
+        }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+    }
+}
