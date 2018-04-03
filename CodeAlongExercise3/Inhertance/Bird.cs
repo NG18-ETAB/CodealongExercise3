67c852b406e6cd29fbd3262547b8d90fc3aa6c47
Ihabo <ihabojr@gmail.com>
2018-04-03 11:00:09 +0200

    Finished codealong on 3.2- Inheritance

---
+++ CodeAlongExercise3/Inhertance/Bird.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3
+{
+    public class Bird : Animal
+    {
+        public Bird(int age, string name, double weight, string plummage) : base(age, name, weight)
+        {
+            Plummage = plummage;
+        }
+
+        public string Plummage { get; set; }
+    }
+}
---
+++ CodeAlongExercise3/Inhertance/Hedgehog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlongExercise3
+{
+    public class Hedgehog : Animal
+    {
+        public Hedgehog(int age, string name, double weight, int numberOfRings) : base(age, name, weight)
+        {
+            NumberOfRings = numberOfRings;
+        }
+
+        public int NumberOfRings { get; set; }
+    }
+}
